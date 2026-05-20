7d6f1c1cbabe7feb89980578b9749d9af8997367
kkennethsieu <kkennethsieu@gmail.com>
2026-05-20 15:23:50 -0700

    Initial Commit

---
+++ Scout/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scout
+//
+//  Created by Kenneth Sieu on 5/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scout/ScoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoutApp.swift
+//  Scout
+//
+//  Created by Kenneth Sieu on 5/20/26.
+//
+
+import SwiftUI
+
+@main
+struct ScoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
